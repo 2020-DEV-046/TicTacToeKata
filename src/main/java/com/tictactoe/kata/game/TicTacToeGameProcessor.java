342e7c0ff0dfa5610f15a081d4d37afc2e241899
AKS291405 <ankitksinghal@rediffmail.com>
2020-02-27 01:33:58 +0530

    Committing partial logic to find winner of Tic Tac Toe game based on same state in any row and column

--- src/main/java/com/tictactoe/kata/game/TicTacToeGameProcessor.java
+++ src/main/java/com/tictactoe/kata/game/TicTacToeGameProcessor.java
@@ -3,7 +3,7 @@ package com.tictactoe.kata.game;
 import org.springframework.stereotype.Component;
 
 /**
- * Tic tac toe game processing class that calculate outcome of game based on
+ * Tic Tac Toe game processing class that calculate outcome of game based on
  * both player choices.
  * 
  * @author AKS1405
@@ -12,9 +12,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class TicTacToeGameProcessor {
 
-	private GAME_STATE[][] board = new GAME_STATE[3][3];	
+	private GAME_STATE[][] board = new GAME_STATE[3][3];
 
-	// Initialize board array with blank state of game
+	private GAME_STATE winner = GAME_STATE.BLANK;
+
+	// Initialize board array with blank state of game in constructor
 	public TicTacToeGameProcessor() {
 		for (int row = 0; row < board.length; row++) {
 			for (int col = 0; col < board[row].length; col++) {
@@ -23,33 +25,147 @@ public class TicTacToeGameProcessor {
 		}
 	}
 
-	// Mark circle in game board and change state of that square to circle
+	/**
+	 * Mark circle in game board and change state of that square to circle and
+	 * return boolean if state changes successfully
+	 * 
+	 * @param row
+	 * @param col
+	 * @return
+	 */
 	public boolean markCircle(int row, int col) {
 		return mark(row, col, GAME_STATE.CIRCLE);
 	}
 
-	// Mark cross in game board and change state of that square to cross
+	/**
+	 * Mark cross in game board and change state of that square to cross and return
+	 * boolean if state changes successfully
+	 * 
+	 * @param row
+	 * @param col
+	 * @return boolean
+	 */
 	public boolean markCross(int row, int col) {
 		return mark(row, col, GAME_STATE.CROSS);
 	}
 
+	/**
+	 * Mark cross in game board and change state of that square to cross and return
+	 * boolean if state changes successfully
+	 * 
+	 * @param row
+	 * @param col
+	 * @param gameState
+	 * @return boolean
+	 */
 	private boolean mark(int row, int col, GAME_STATE gameState) {
 		if (board[row][col] == GAME_STATE.BLANK) {
 			board[row][col] = gameState;
+			
+			// Checks whether winner has been found post every players chance of marking
+			setWinner(row, col, gameState);
 			return true;
 		}
 		return false;
 	}
-	
+
+	/**
+	 * This method finds winner after each player plays and marks O or X based on
+	 * condition of winner based on Tic Tac Toe game rules
+	 * 
+	 * @param row
+	 * @param col
+	 * @param gameState
+	 */
+	public void setWinner(int row, int col, GAME_STATE gameState) {
+		validateThreeSameStateInColumn(col, gameState);
+		validateThreeSameStateInRow(row, gameState);
+	}
+
+	/**
+	 * This method returns state of winner of any time
+	 * 
+	 * @return GAME_STATE the winner
+	 */
+	public GAME_STATE getWinner() {
+		return this.winner;
+	}
+
+	/**
+	 * This method finds that if one row has same sign and changes winner state to
+	 * that sign like O or X
+	 * 
+	 * @param row
+	 * @param gameState
+	 */
+	private void validateThreeSameStateInRow(int row, GAME_STATE gameState) {
+		for (int col = 0; col < board.length; col++) {
+			if (board[row][col] != gameState) {
+				break;
+			}
+
+			if (col == 2) {
+				winner = gameState;
+			}
+		}
+	}
+
+	/**
+	 * This method finds that if one column has same sign and changes winner state to
+	 * that sign like O or X
+	 * 
+	 * @param col
+	 * @param gameState
+	 */
+	private void validateThreeSameStateInColumn(int col, GAME_STATE gameState) {
+		for (int row = 0; row < board.length; row++) {
+			if (board[row][col] != gameState) {
+				break;
+			}
+
+			if (row == 2) {
+				winner = gameState;
+			}
+		}
+	}
+
+	/**
+	 * This method gives current state of board at any time
+	 * 
+	 * @return GAME_STATE[][] 
+	 */
 	public GAME_STATE[][] getBoard() {
 		return this.board;
 	}
 
+	/**
+	 * This method finds whether at any location on board is marked with circle or not
+	 * @param row
+	 * @param col
+	 * @return boolean
+	 */
 	public boolean isCircle(int row, int col) {
 		return board[row][col] == GAME_STATE.CIRCLE;
 	}
 
+	/**
+	 * This method finds whether at any location on board is marked with circle or not
+	 * @param row
+	 * @param col
+	 * @return boolean
+	 */
 	public boolean isCross(int row, int col) {
 		return board[row][col] == GAME_STATE.CROSS;
-	}	
+	}
+	
+	/**
+	 * This method used to check whether winner has been found after every chance played by player.
+	 * If state of winner is changed from Blank to Cicrle and Cross we can say that one of player 
+	 * has won the game.
+	 * 
+	 * @return boolean
+	 */
+	public boolean isFinished() {
+		return this.winner != GAME_STATE.BLANK;
+	}
 }
--- src/test/java/com/tictactoe/kata/TicTacToeKataApplicationTests.java
+++ src/test/java/com/tictactoe/kata/TicTacToeKataApplicationTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.tictactoe.kata.game.GAME_STATE;
 import com.tictactoe.kata.game.TicTacToeGameProcessor;
 
 /**
@@ -19,64 +20,82 @@ public class TicTacToeKataApplicationTests {
 	public void setUp() {
 		ticTacToe = new TicTacToeGameProcessor();
 	}
-	
+
 	@Test
-    public void testCircleMarking() {
+	public void testCircleMarking() {
 		ticTacToe.markCircle(0, 1);
 
-        assertEquals(true, ticTacToe.isCircle(0, 1));
-    }
-	
-	@Test
-    public void testMarkCircleOnBlank() {
-        boolean circlePlaced = ticTacToe.markCircle(1, 2);
+		assertEquals(true, ticTacToe.isCircle(0, 1));
+	}
 
-        assertEquals(true, circlePlaced);
-    }
-	
 	@Test
-    public void testMarkingCircleOverExistingMarkedCircle() {
+	public void testMarkCircleOnBlank() {
+		boolean circlePlaced = ticTacToe.markCircle(1, 2);
+
+		assertEquals(true, circlePlaced);
+	}
+
+	@Test
+	public void testMarkingCircleOverExistingMarkedCircle() {
 		ticTacToe.markCircle(2, 2);
-        boolean circlePlaced = ticTacToe.markCircle(2, 2);
+		boolean circlePlaced = ticTacToe.markCircle(2, 2);
 
-        assertEquals(false, circlePlaced);
-    }
-	
+		assertEquals(false, circlePlaced);
+	}
+
 	@Test
-    public void testMarkingCircleOverExistingMarkedCross() {
+	public void testMarkingCircleOverExistingMarkedCross() {
 		ticTacToe.markCross(2, 2);
-        boolean circlePlaced = ticTacToe.markCircle(2, 2);
+		boolean circlePlaced = ticTacToe.markCircle(2, 2);
 
-        assertEquals(false, circlePlaced);
-    }
-	
+		assertEquals(false, circlePlaced);
+	}
+
 	@Test
-    public void testCrossMarking() {
+	public void testCrossMarking() {
 		ticTacToe.markCross(0, 1);
 
-        assertEquals(true, ticTacToe.isCross(0, 1));
-    }
-	
-	@Test
-    public void testMarkCrossOnBlank() {
-        boolean crossPlaced = ticTacToe.markCircle(1, 2);
+		assertEquals(true, ticTacToe.isCross(0, 1));
+	}
 
-        assertEquals(true, crossPlaced);
-    }
-	
 	@Test
-    public void testMarkingCrossOverExistingMarkedCross() {
+	public void testMarkCrossOnBlank() {
+		boolean crossPlaced = ticTacToe.markCircle(1, 2);
+
+		assertEquals(true, crossPlaced);
+	}
+
+	@Test
+	public void testMarkingCrossOverExistingMarkedCross() {
 		ticTacToe.markCross(2, 1);
-        boolean crossPlaced = ticTacToe.markCross(2, 1);
+		boolean crossPlaced = ticTacToe.markCross(2, 1);
 
-        assertEquals(false, crossPlaced);
-    }
+		assertEquals(false, crossPlaced);
+	}
+
+	@Test
+	public void testMarkingCrossOverExistingMarkedCircle() {
+		ticTacToe.markCircle(2, 1);
+		boolean crossPlaced = ticTacToe.markCross(2, 1);
+
+		assertEquals(false, crossPlaced);
+	}
+
+	@Test
+	public void testThreeCircleMarkedInARowWins() {
+		ticTacToe.markCircle(0, 0);
+		ticTacToe.markCircle(0, 1);
+		ticTacToe.markCircle(0, 2);
+		assertEquals(GAME_STATE.CIRCLE, ticTacToe.getWinner());
+		assertEquals(true, ticTacToe.isFinished());
+	}
 	
 	@Test
-    public void testMarkingCrossOverExistingMarkedCircle() {
-		ticTacToe.markCircle(2, 1);
-        boolean crossPlaced = ticTacToe.markCross(2, 1);
-
-        assertEquals(false, crossPlaced);
+    public void testThreeCircleMarkedInAColWins() {
+    	ticTacToe.markCircle(0, 0);
+    	ticTacToe.markCircle(1, 0);
+    	ticTacToe.markCircle(2, 0);
+    	assertEquals(GAME_STATE.CIRCLE, ticTacToe.getWinner());
+        assertEquals(true, ticTacToe.isFinished());
     }
 }
